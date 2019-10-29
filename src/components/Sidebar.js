37c8500d2d15d1c867da3162d002631ea7f6eec6
Chris Morrell <inxilpro@users.noreply.github.com>
2019-10-29 12:58:56 -0400

    Fixed GraphQL query

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,7 +7,7 @@ export default function Sidebar() {
             allGithubData {
                 nodes {
                     data {
-                        viewer {
+                        user {
                             starredRepositories {
                                 nodes {
                                     id,
@@ -26,16 +26,16 @@ export default function Sidebar() {
         }
 	`);
 	
-	const repos = data.allGithubData.nodes[0].data.viewer.starredRepositories.nodes;
+	const repos = data.allGithubData.nodes[0].data.user.starredRepositories.nodes;
 	
 	return (
-		<div className="lg:pl-12 lg:-mr-4 lg:pr-4 lg:-my-4 lg:py-8 lg:opacity-50 hover:opacity-100 transition transition-opacity h-screen sticky top-0 overflow-y-auto overflow-x-hidden text-base leading-none shadow-overflow-y">
+		<div className="my-12 lg:pl-12 lg:-mr-4 lg:pr-4 lg:-my-4 lg:py-8 lg:opacity-50 hover:opacity-100 transition transition-opacity lg:h-screen lg:sticky lg:top-0 overflow-y-auto overflow-x-hidden text-base leading-none shadow-overflow-y">
 			<h2 className="text-3xl font-bold font-slant text-gray-900 mb-8">
 				Interesting&hellip;
 			</h2>
 			
 			{ repos.map(({ shortDescriptionHTML, name, owner, url, id }) => (
-				<div key={ id } className="mb-4">
+				<div key={ id } className="mb-6">
 					<h3 className="mb-1">
 						<a href={url} target="_blank" rel="noopener noreferrer" className="text-xl tracking-wider font-bold font-slant group">
 							<span className="text-gray-600 group-hover:text-blue-700">
@@ -53,6 +53,14 @@ export default function Sidebar() {
 					   dangerouslySetInnerHTML={ { __html: shortDescriptionHTML } } />
 				</div>
 			)) }
+			
+			<a
+				className="block my-12 pt-6 border-t text-center text-2xl tracking-wider font-bold font-slant opacity-75 hover:opacity-100"
+				href="https://github.com/inxilpro?tab=stars"
+				target="_blank"
+				rel="noopener noreferrer"
+				children="More…"
+			/>
 		</div>
 	);
 }
