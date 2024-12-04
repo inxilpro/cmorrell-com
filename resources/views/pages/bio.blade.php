cecbcdcdbce28200ced43b4ba0e4ece9b110da86
Chris Morrell <inxilpro@users.noreply.github.com>
2024-12-04 15:39:56 -0500

    Bio

---
+++ resources/views/pages/bio.blade.php
@@ -0,0 +1,37 @@
+<x-layout>
+	
+	<div class="flex justify-start items-center gap-6 mt-12">
+		<video class="w-full h-auto max-w-xs rounded-lg aspect-[9/16] object-cover hidden sm:block" autoplay loop muted playsinline>
+			<source src="{{ asset('images/cm.mp4') }}" type="video/mp4" />
+		</video>
+		<div>
+			<h1 class="text-5xl lg:text-6xl font-bold font-slant text-gray-800">
+				Chris Morrell
+			</h1>
+			
+			<p class="text-xl lg:text-2xl leading-normal my-4">
+				Chris is a father, the CEO and CTO of the International Association of Certified Home
+				Inspectors (InterNACHI), and an avid open-source contributor. He's been building
+				tools to help home inspectors for 25+ years, and tries to contribute as much
+				of his work back to the open source community as possible.
+			</p>
+			
+			<div class="mt-12">
+				<a 
+					href="{{ asset('images/headshot.zip') }}"
+					class="inline-flex items-center gap-x-2 rounded-md bg-gray-100 px-3.5 py-2.5 text-2xl font-slant text-black shadow-sm hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+					download
+				>
+					<img
+						class="w-12 h-12 rounded-full"
+						alt="Chris Morrell"
+						src="{{ asset('images/cm-headshot-for-web.jpg') }}"
+					/>
+					Download Headshot
+				</a>
+			</div>
+		
+		</div>
+	</div>
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ Route::view('/mastodon', 'pages.mastodon');
 Route::view('/models-in-verbs', 'pages.models-in-verbs');
 Route::view('/verbs-errors', 'pages.verbs-errors');
 Route::view('/joey-t-calculator', 'pages.joey-t-calculator');
+Route::view('/bio', 'pages.bio');
 
 if (App::isLocal()) {
 	Route::view('opengraph', 'opengraph');
