11eead8902cdd9c991a173596bfd84769959e643
Shift <shift@laravelshift.com>
2025-08-22 15:19:33 +0000

    Consolidate service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 use App\Support\TorchlightManager;
+use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Contracts\Support\DeferringDisplayableValue;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
@@ -12,6 +15,15 @@ use Torchlight\Manager;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * The path to your application's "home" route.
+     *
+     * Typically, users are redirected here after authentication.
+     *
+     * @var string
+     */
+    public const HOME = '/home';
+
     public function register(): void
     {
         //
@@ -40,6 +52,15 @@ class AppServiceProvider extends ServiceProvider
                     default => Str::slug(strtolower($path)),
                 };
             }
+        });
+
+        $this->bootRoute();
+    }
+
+    public function bootRoute(): void
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
     }
 }
